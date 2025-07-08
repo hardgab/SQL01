73f0e4f4a0e387ac313171eb32058680857d1645
hardgab <gabrielsousa.gss722@gmail.com>
2025-07-08 17:26:55 -0300

    Delete SQL Gabs directory

--- SQL Gabs/Consulta01.sql
+++
@@ -1,8 +0,0 @@
---- selecionar colunas que eu quero
-select p.id_pedido, p.descricao, p.valor, c.Id_cliente, c.nome, c.cidade from tb_pedidos p
---- Junção das colunas de tabelas distintas
-JOIN 
-	tb_clientes c on p.fk_cliente =  c.Id_cliente
-
---- ordenar
-order by c.nome;
